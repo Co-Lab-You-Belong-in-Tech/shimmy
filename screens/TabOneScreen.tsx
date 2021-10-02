bbaae9209beb94510fb4b38f0b0aa796aca02a98
unknown <pjb9299@hotmail.com>
2021-10-02 11:29:08 -0400

    test commit

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Button, Alert } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
@@ -8,7 +7,11 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Hello World</Text>
+      <Text>React Native things</Text>
+      <Button
+      onPress={()=>{Alert.alert("Alerted")}}
+      title="LearnMore"/>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
     </View>
