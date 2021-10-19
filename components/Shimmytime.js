402956e807eef32b1e4193355b7b586e6da0e586
christian <rewindforward@gmail.com>
2021-10-18 22:01:11 -0400

    Fixes counter

--- components/Shimmytime.js
+++ components/Shimmytime.js
@@ -4,39 +4,34 @@ import { HeaderBackButton } from '@react-navigation/elements';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer';
 import SvgComponent from './SvgComponent';
 
-
 const Shimmytime = ({navigation}) => {
-    const [isPlaying, setIsPlaying] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(true);
 
-    navigation.setOptions({
-        headerLeft: () => <HeaderBackButton onPress={() => navigation.navigate('Home')}/>
-        
-    })
+  navigation.setOptions({
+    headerLeft: () => <HeaderBackButton onPress={() => navigation.navigate('Home')}/>
+  })
 
-    return (
-        <View style={styles.container}>
-            <Text>Move in a way that feels good to you!</Text>
-            <SvgComponent style={{position: 'absolute', marginBottom: 20}}/>
-            <CountdownCircleTimer
-                isPlaying={isPlaying}
-                duration={60}
-                colors='#FFA332'
-                rotation='counterclockwise'
-                onComplete={() => [true]}
-                size={300}
-                >
-                    
-                    {({ remainingTime }) => {
-                        const minutes = Math.floor(remainingTime / 60);
-                        const seconds = remainingTime % 60;
-
-                        return  <Text style={{marginTop:'70%'}}>{`${minutes}:${seconds}`}</Text>
-                    }}
-            </CountdownCircleTimer>
-            <Text>Music title go here</Text>
-        </View>
-        
-    )
+  return (
+       <View style={styles.container}>
+          <Text>Move in a way that feels good to you!</Text>
+          <SvgComponent style={{position: 'absolute', marginBottom: 20}} />
+          <CountdownCircleTimer
+              isPlaying={isPlaying}
+              duration={60}
+              colors='#FFA332'
+              rotation='counterclockwise'
+              onComplete={() => [true]}
+              size={300}
+              >
+              {({ remainingTime }) => {
+                const minutes = Math.floor(remainingTime / 60);
+                const seconds = remainingTime % 60;
+                return <Text style={{marginTop:'70%'}}>{`${minutes < 1 ? 0 : 1 }:${seconds}`}</Text>
+              }}
+          </CountdownCircleTimer>
+          <Text>Music title go here</Text>
+      </View>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -47,6 +42,5 @@ const styles = StyleSheet.create({
       backgroundColor: '#ecf0f1',
       padding: 8,
     },
-
   });
 export default Shimmytime;
